973638cb9ca60d88d71b672ded190f38e9deec1f
Daniel <btt.bajomo@gmail.com>
2023-02-16 22:51:48 +0000

    First Build

---
+++ vida/src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+import NavBar from "./components/navBar.vue";
+</script>
+
+<template>
+  <NavBar/>
+  
+  
+  
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <router-link class="navbar-brand handwritten navElements" to="/auction101">Auction101</router-link>
+            <router-link class="navbar-brand navElements ml-auto" to="/createItem">Create Item</router-link>
+            <router-link class="navbar-brand navElements ml-auto" to="/profile">Profile</router-link>
+            <router-link class="navbar-brand navElements ml-auto" to="/userItems">Your Items</router-link>
+            <router-link class="navbar-brand navElements ml-auto" to="/itemsWon">Items Won</router-link>
+            
+        </div>
+</template>
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ vida/src/components/navBar.vue
@@ -0,0 +1,68 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+</script>
+<template>
+
+<nav class="navbar navbar-expand-lg navbar-dark border-bottom ">
+  
+  <a class="navbar-brand my-40" href="#"><img src="./assets/Vida_txt.png" class="logo" alt="Vite logo" /></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    
+  </button>
+
+  <div class="collapse navbar-collapse position-absolute  end-0" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <router-link class="nav" to="/auction101">Home</router-link>
+            
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>          
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      
+    </ul>
+    
+  </div>
+
+</nav>
+</template>
+
+<style scoped>
+
+nav {
+    background-color: #242424;
+    display: flex;
+}
+
+.logo {
+  width:70px;
+  height: auto;
+  position: relative;
+  left: 10px;
+}
+.handwritten {
+    font-family: Pacifico, cursive;
+}
+
+.navElements {
+    font-size: 144%;
+    text-decoration: none;
+}
+</style>
+
